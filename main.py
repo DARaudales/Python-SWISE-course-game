d28c87ee582ef431b397e8712321c61b26943437
Helga Ordoñez <ajanariba@outlook.com>
2020-09-15 05:07:43 -0600

    Agregué nuevas funciones, corregí lvl_up

--- main.py
+++ main.py
@@ -1,6 +1,43 @@
 import pandas as pd
 import numpy as np
-from pprint import pprint
+import pickle
+
+def game_over(estado_ganador,play):
+    if estado_ganador:
+        print("¿Te gustaría jugar de nuevo?\n1.Sí\n2.No")
+        while True:
+            try:
+                opción=int(input("Opción: "))
+            except ValueError:
+                pass
+            else:
+                if opción not in [1,2]:
+                    pass
+                elif opción==1:
+                    play=True
+                    break
+                else:
+                    play=False
+                    break
+
+    else:
+        print("FIN DEL JUEGO\n¿Qué te gustaría hacer?")
+        while True:
+            try:
+                opción = int(input("Opción: "))
+            except ValueError:
+                pass
+            else:
+                if opción not in [1,2]:
+                    pass
+                elif opción == 1:
+                    play = True
+                    break
+                else:
+                    play = False
+                    break
+    return play
+
 
 nombres=["Ábner","Ángel","David","María"]
 hps=[36,36,36,36]
@@ -17,24 +54,9 @@ dic_héroes= {key:value for key, value in zip(key_héroes,value_héroes)}
 
 
 df_héroes=pd.DataFrame(dic_héroes)
+df_héroes.Carisma=df_héroes.Carisma.astype(float)
+df_héroes.Inteligencia=df_héroes.Inteligencia.astype(float)
 print(df_héroes)
-
-print("Te damos la bienvenida a esta aventura.")
-print("En este juego, lucharás contra las fuerzas del mal para rescatar tu índice acaémico.")
-print("Elige como quién quieres jugar:")
-print("1.Ábner\n2.Ángel\n3.David\n4.María")
-while True:
-  try:
-    n=int(input("Opción: "))
-  except ValueError:
-    pass
-  else:
-    if 1<=n<=4:
-      n-=1
-      break
-    else:
-      pass
-
 
 class Personaje:
 
@@ -53,8 +75,9 @@ class Villano(Personaje):
 
 class Héroe(Personaje):
 
-  def __init__(self, nombre=df_héroes.Nombre[n], hp=36, stats=None):
+  def __init__(self, nombre=None, hp=36, stats=None):
     Personaje.__init__(self, nombre, hp)
+    self.nombre = df_héroes.Nombre[n] if nombre is None else nombre
     self.stats={
                 "Carisma": df_héroes.Carisma[n],
                 "Inteligencia":df_héroes.Inteligencia[n],
@@ -62,60 +85,108 @@ class Héroe(Personaje):
                 "Dinero": df_héroes.Dinero[n],
                 "Nivel": df_héroes.Nivel[n]
                 } if stats is None else stats
+    self.skills=[]
 
-  def lvlup(self):
-    df_héroes.Carisma[n]+=0.5
-    df_héroes.Inteligencia[n]+=0.5
-    df_héroes.Energía[n]=10
-    df_héroes.Dinero[n]+=50
-    df_héroes.Nivel[n]+=1
+  def get_skill(self, skill):
+      self.skills.append(skill)
 
-player=Héroe()
-Urrutia=Villano("Carlos Urrutia")
-Mark=Villano("Androide Mark III")
+  def lvl_up(self):
+    df_héroes.loc[n,"Carisma"]+=0.5
+    df_héroes.loc[n,"Inteligencia"]+=0.5
+    df_héroes.loc[n,"Dinero"]+=50
+    df_héroes.loc[n,"Nivel"]+=1
+    self.replenish()
+    self.__init__()
 
-player.lvlup()
-print(df_héroes)
+  def replenish(self):
+      df_héroes.loc[n,"Energía"]=10
+      df_héroes.loc[n, "HP"]=36
+      df_héroes.loc[n, "CM"]=0
 
-r1=np.random.randint(1,11)
-r2=np.random.randint(1,11)
-coeficientes = np.poly([r1,r2])
-f=np.poly1d(coeficientes)
+play=True
 
+while play:
 
-
-print(f"A ver si sabe factorizar, deme uno de los ceros de este polinomio:\n\n{f}")
-if r1==r2:
-    print("Uy, es una mujer desnuda.")
-try:
-    y=int(input("\nRespuesta: "))
-except ValueError:
-    print("Le dije que mirara su cuaderno, coño")
-    pregunta1=False
-else:
-    if y in [r1,r2]:
-        print("Muy bien, cuando se gradúe me da su autógrafo")
-        pregunta1=True
-    else:
-      print("Le dije que mirara su cuaderno, coño")
-      pregunta1=False
-
-if pregunta1:
-    x=np.random.randint(-6,6)
-    print(f"\nBueno, a ver si sabe evaluar una función. Probemos con el mismo de la Pregunta 1:\n\nf(x)=\n{f}")
-    print(f"\nEncuentre f({x})")
-    try:
-        y=int(input(f"f({x})= "))
-    except ValueError:
-        print("Beh, no sabe que tiene que meter números.")
-        pregunta2=False
-    else:
-        if y==f(x):
-            print("Pucha, sí lee su cuaderno.")
-            pregunta2=True
+    print("Te damos la bienvenida a esta aventura.")
+    print("En este juego, lucharás contra las fuerzas del mal para rescatar tu índice acaémico.")
+    print("Elige como quién quieres jugar:")
+    print("1.Ábner\n2.Ángel\n3.David\n4.María")
+    while True:
+        try:
+            n=int(input("Opción: "))
+        except ValueError:
+            pass
         else:
-            print(f"Mire, que la respuesta es {y} dice JAJA.")
-            pregunta2=False
+            if 1<=n<=4:
+                n-=1
+                break
+            else:
+                pass
 
-if pregunta2:
-    print("Felicidades, siga estudiando, ahora se viene Cálculo I, métala con Donaire.")
+    player=Héroe()
+    completado=False
+
+#Parte 1: Lord Borjas, 110
+
+    repitencias=0
+    aprobado=False
+
+    while repitencias<3 and aprobado==False:
+
+        pregunta1 = False
+        pregunta2 = False
+        r1 = np.random.randint(1, 11)
+        r2 = np.random.randint(1, 11)
+        coeficientes = np.poly([r1, r2])
+        f = np.poly1d(coeficientes)
+
+        print(f"A ver si sabe factorizar, deme uno de los ceros de este polinomio:\n\n{f}")
+        if r1==r2:
+            print("Uy, es una mujer desnuda.")
+        try:
+            y=int(input("\nRespuesta: "))
+        except ValueError:
+            print("Le dije que mirara su cuaderno, coño")
+            repitencias+=1
+        else:
+            if y in [r1,r2]:
+                print("Pucha, sí lee su cuaderno.")
+                pregunta1=True
+            else:
+              print("Le dije que mirara su cuaderno, coño")
+              repitencias+=1
+
+        if pregunta1:
+            x=np.random.randint(-6,6)
+            print(f"\nBueno, a ver si sabe evaluar una función. Probemos con el mismo de la Pregunta 1:\n\nf(x)=\n{f}")
+            print(f"\nEncuentre f({x})")
+            try:
+                y=int(input(f"f({x})= "))
+            except ValueError:
+                print("Beh, no sabe que tiene que meter números.")
+                repitencias+=1
+            else:
+                if y==f(x):
+                    print("Muy bien, cuando se gradúe me da su autógrafo.")
+                    pregunta2=True
+                    aprobado=True
+                else:
+                    print(f"Mire, que la respuesta es {y} dice JAJA.")
+
+        if aprobado:
+            print("Felicidades, siga estudiando, ahora se viene Cálculo I, métala con Donaire.")
+            player.lvl_up()
+        else:
+            if repitencias==1:
+                print("Bueno, la siguiente vez que lleve la clase nos vemos.")
+            elif repitencias==2:
+                print("Póngase las pilas, sólo una vez más puede llevar la clase.")
+            else:
+                print("Pues, siempre puede irse a estudiar Admin.")
+                play=game_over(completado,play)
+
+#Parte 2: Donaire, Cálculo I
+#Parte 3: Urrutia, Cálculo II
+    urrutia=Villano("Carlos Urrutia")
+#Parte 4: Mark, Batalla Final (Repo Cálculo II)
+    mark=Villano("Androide Mark III")
